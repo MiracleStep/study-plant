3e6f7131977d5dc4ed7d3c8d675b401c336988f1
suxiang <2585546823@qq.com>
2024-07-03 16:35:28 +0800

    开发互动问题相关接口：新增回答或评论、客户端-分页查询回答或评论

--- tj-learning/src/main/java/com/tianji/learning/service/IInteractionReplyService.java
+++ tj-learning/src/main/java/com/tianji/learning/service/IInteractionReplyService.java
@@ -1,7 +1,11 @@
 package com.tianji.learning.service;
 
+import com.tianji.common.domain.dto.PageDTO;
+import com.tianji.learning.domain.dto.ReplyDTO;
 import com.tianji.learning.domain.po.InteractionReply;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.tianji.learning.domain.query.ReplyPageQuery;
+import com.tianji.learning.domain.vo.ReplyVO;
 
 /**
  * <p>
@@ -13,4 +17,17 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IInteractionReplyService extends IService<InteractionReply> {
 
+    /**
+     * 新增回答或评论
+     * @param dto
+     */
+    void saveReply(ReplyDTO dto);
+
+    /**
+     * 分页查询回答或评论
+     * @param pageQuery
+     * @param b
+     * @return
+     */
+    PageDTO<ReplyVO> queryReplyPage(ReplyPageQuery pageQuery, boolean b);
 }
